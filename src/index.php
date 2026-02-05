37dae229634f57ff51e9bac6287140344ecf16fe
ReasonalWork <reasonalworkplace@gmail.com>
2026-02-05 16:09:29 +0800

    Added new Folders & files

--- src/index.php
+++ src/index.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <div class="navigation-bar">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+    </div>
+    <div class="content-container">
+        
+    </div>
 </body>
 </html>
