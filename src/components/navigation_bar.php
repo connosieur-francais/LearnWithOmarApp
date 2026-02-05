b36db23f94453ee027f0cb1bf883b634b9ee9b32
ReasonalWork <reasonalworkplace@gmail.com>
2026-02-05 16:58:38 +0800

    Removed Bullets from nav bar list

--- src/components/navigation_bar.php
+++ src/components/navigation_bar.php
@@ -1,5 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navigation Bar</title>
+</head>
 
-<ul>
-    <li><a href="LearnWithOmarApp\src\index.php">Home</a></li>
-</ul>
+<body>
+    <ul>
+        <li><a href="LearnWithOmarApp\src\index.php">Home</a></li>
+    </ul>
+</body>
+
+</html>
