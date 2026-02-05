a9783240843af689d749e5c8674a72280f446e61
ReasonalWork <reasonalworkplace@gmail.com>
2026-02-05 16:49:00 +0800

    Arranged Files

---
+++ src/components/navigation_bar.php
@@ -0,0 +1,5 @@
+
+
+<ul>
+    <li><a href="LearnWithOmarApp\src\index.php">Home</a></li>
+</ul>
--- src/index.php
+++ src/index.php
@@ -4,14 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>LearnWithOmar</title>
 </head>
 
 <body>
-    <div class="navigation-bar">
-        <a href="#home">Home</a>
-        <a href="#about">About</a>
-    </div>
+    
     <div class="content-container">
         
     </div>
