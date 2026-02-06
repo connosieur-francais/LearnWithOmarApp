39a28a2714ec689b99eeb68cfdc06520f68c9153
Omar Hussein <omariz201184@gmail.com>
2026-02-06 15:50:17 +0200

    Added Contact and About pages

--- src/components/navigation_bar.php
+++ src/components/navigation_bar.php
@@ -10,6 +10,8 @@
 <body>
     <ul>
         <li><a href="LearnWithOmarApp\src\index.php">Home</a></li>
+        <li><a href="LearnWithOmarApp\src\pages\about.php">About</a></li>
+        <li><a href="LearnWithOmarApp\src\pages\contact.php">Contact</a></li>
     </ul>
 </body>
 
