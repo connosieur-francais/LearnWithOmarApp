0594045e4cb97673e9e0c2d6395fb4b2f4960473
ReasonalWork <reasonalworkplace@gmail.com>
2026-02-06 20:23:46 +0800

    little style change: added background gradient to body in index.php and fixed include path.

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
-    include $_SERVER['DOCUMENT_ROOT'].'/src/setup/conn.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/src/setup/conn.php';
 ?>
 
 <!DOCTYPE html>
@@ -9,9 +9,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LearnWithOmar</title>
+    <style>
+        body {
+            width: 1440px;
+            height: 1024px;
+            background: var(--Gradients-Light-Blue-1, linear-gradient(135deg, #176AB1 0%, #CBD8F1 100%));
+        }
+    </style>
 </head>
 
 <body>
-    <?php include $_SERVER['DOCUMENT_ROOT'].'/src/components/navigation_bar.php'; ?>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/src/components/navigation_bar.php'; ?>
 </body>
+
 </html>
