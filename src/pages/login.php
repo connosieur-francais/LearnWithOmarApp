838b83d076c6b5d5ab8f33923b5b7a8c6b7e6156
ReasonalWork <reasonalworkplace@gmail.com>
2026-02-09 20:09:38 +0800

    Login Page Completed with Title, Subtitle & SVG Icon

--- src/pages/login.php
+++ src/pages/login.php
@@ -1,11 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login to LearnWithOmar</title>
+    <link rel="stylesheet" href="/LearnWithOmarApp/src/css/login.css">
 </head>
+
 <body>
-    <h1>Login Page</h1>
+    <div class="container">
+        <div class="login-form">
+            <div class="title-container">
+                <h1 id="title">Log In</h1>
+                <div class="subtitle">
+                    <div name="logo">
+                        <object data="/LearnWithOmarApp/src/icons/graduationhat.svg" type="image/svg+xml"></object>
+                    </div>
+                    <p id="subtitle">LearnWithOmar</p>
+                </div>
+            </div>
+            <div class="form-container">
+                <!-- Form Elements -->
+
+            </div>
+        </div>
+        <div class="footer-container">
+            <!-- Footer -->
+            <p>Footer</p>
+        </div>
 </body>
+
 </html>
